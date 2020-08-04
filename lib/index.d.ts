ef272462d0b1015a8ac80573fbab0577bcf04e38
jovi <linjuewei@corp.netease.com>
2020-08-04 19:45:40 +0800

    feat: build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,7 @@
+declare const SequelizeAuto: any;
+export default class SequelizeAutoEgg extends SequelizeAuto {
+    constructor(database: string, username: string, password: string, options: any);
+    write(attributes: any, typescriptFiles: any, callback: any): void;
+}
 export declare function genEggSequelize(): void;
+export {};
