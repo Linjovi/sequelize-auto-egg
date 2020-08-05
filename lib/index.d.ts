462ca4f510360dc29715247862359525030ddec6
jovi <linjuewei@corp.netease.com>
2020-08-05 14:54:46 +0800

    feat: mysql2

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,5 +3,5 @@ export default class SequelizeAutoEgg extends SequelizeAuto {
     constructor(database: string, username: string, password: string, options: any);
     write(attributes: any, typescriptFiles: any, callback: any): void;
 }
-export declare function genEggSequelize(): void;
+export declare function genEggSequelize(dbName: string | undefined, user: string | undefined, password: string | undefined, options: any): void;
 export {};
--- lib/index.ts
+++ lib/index.ts
@@ -16,14 +16,16 @@ export default class SequelizeAutoEgg extends SequelizeAuto{
   }
 }
 
-export function genEggSequelize() {
-  const auto = new SequelizeAutoEgg("crmdb", "root", "12345", {
-    host: "127.0.0.1",
-    dialect: "mysql",
-    port: "3306",
-    tables: ["ykt_course_income"],
-    camelCase: true,
-  });
+export function genEggSequelize(dbName="crmdb",user="root",password="12345",options:any) {
+  const auto = new SequelizeAutoEgg(dbName, user, password, options
+    // {
+    // host: "127.0.0.1",
+    // dialect: "mysql",
+    // port: "3306",
+    // tables: ["ykt_course_income"],
+    // camelCase: true,
+    // }
+  );
   auto.run(function (err: any) {
     console.log(auto.eggModel)
   });
